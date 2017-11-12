de5d0ba8b671b9044db26de7381cb26f0a6051dd
cigink <cigink@gmail.com>
2017-11-12 12:54:56 +0100

    First commit

---
+++ Wallet.py
@@ -0,0 +1,19 @@
+# Python 3
+# this is a test program to store passwords
+
+PASSWORDS = {'facebook' : 'Password',
+             'gmail' : 'Password2'
+             }
+
+import sys
+import pyperclip
+if len(sys.argv)<2 :
+    print ('Using python to copy password')
+    sys.exit()
+
+account = sys.argv[1]
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print ('Password to account copied to clipboard')
+else :
+    print ('Account not found')
